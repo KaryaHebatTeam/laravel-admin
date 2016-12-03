0280afee940dcfaab98d436586896abcb9b469f6
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2016-12-03 09:28:44 +0700

    Vendor deleted

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * MainController
+ *
+ * @author Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
+ * 
+ */
+
+class MainController extends Controller
+{
+	public function index()
+	{
+		# code...
+	}
+}
