fb75bd95941d3ad03fae27e245180f0413d46e92
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2017-01-07 12:24:07 +0700

    AdminLTE theme navigation fix

--- packages/greatworks/menular/src/views/layout_adminlte.blade.php
+++ packages/greatworks/menular/src/views/layout_adminlte.blade.php
@@ -336,8 +336,8 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li class="active"><a href="{{ url('adminlte/index.html') }}"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+            <li><a href="{{ url('adminlte/index2.html') }}"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -349,14 +349,14 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-            <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+            <li><a href="{{ url('adminlte/pages/layout/top-nav.html') }}"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+            <li><a href="{{ url('adminlte/pages/layout/boxed.html') }}"><i class="fa fa-circle-o"></i> Boxed</a></li>
+            <li><a href="{{ url('adminlte/pages/layout/fixed.html') }}"><i class="fa fa-circle-o"></i> Fixed</a></li>
+            <li><a href="{{ url('adminlte/pages/layout/collapsed-sidebar.html') }}"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
           </ul>
         </li>
         <li>
-          <a href="pages/widgets.html">
+          <a href="{{ url('adminlte/pages/widgets.html') }}">
             <i class="fa fa-th"></i> <span>Widgets</span>
             <span class="pull-right-container">
               <small class="label pull-right bg-green">new</small>
@@ -372,10 +372,10 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+            <li><a href="{{ url('adminlte/pages/charts/chartjs.html') }}"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+            <li><a href="{{ url('adminlte/pages/charts/morris.html') }}"><i class="fa fa-circle-o"></i> Morris</a></li>
+            <li><a href="{{ url('adminlte/pages/charts/flot.html') }}"><i class="fa fa-circle-o"></i> Flot</a></li>
+            <li><a href="{{ url('adminlte/pages/charts/inline.html') }}"><i class="fa fa-circle-o"></i> Inline charts</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -387,12 +387,12 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
-            <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
-            <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
-            <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
-            <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
-            <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
+            <li><a href="{{ url('adminlte/pages/UI/general.html') }}"><i class="fa fa-circle-o"></i> General</a></li>
+            <li><a href="{{ url('adminlte/pages/UI/icons.html') }}"><i class="fa fa-circle-o"></i> Icons</a></li>
+            <li><a href="{{ url('adminlte/pages/UI/buttons.html') }}"><i class="fa fa-circle-o"></i> Buttons</a></li>
+            <li><a href="{{ url('adminlte/pages/UI/sliders.html') }}"><i class="fa fa-circle-o"></i> Sliders</a></li>
+            <li><a href="{{ url('adminlte/pages/UI/timeline.html') }}"><i class="fa fa-circle-o"></i> Timeline</a></li>
+            <li><a href="{{ url('adminlte/pages/UI/modals.html') }}"><i class="fa fa-circle-o"></i> Modals</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -403,9 +403,9 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
-            <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-            <li><a href="pages/forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
+            <li><a href="{{ url('adminlte/pages/forms/general.html') }}"><i class="fa fa-circle-o"></i> General Elements</a></li>
+            <li><a href="{{ url('adminlte/pages/forms/advanced.html') }}"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
+            <li><a href="{{ url('adminlte/pages/forms/editors.html') }}"><i class="fa fa-circle-o"></i> Editors</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -416,12 +416,12 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
-            <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
+            <li><a href="{{ url('adminlte/pages/tables/simple.html') }}"><i class="fa fa-circle-o"></i> Simple tables</a></li>
+            <li><a href="{{ url('adminlte/pages/tables/data.html') }}"><i class="fa fa-circle-o"></i> Data tables</a></li>
           </ul>
         </li>
         <li>
-          <a href="pages/calendar.html">
+          <a href="{{ url('adminlte/pages/calendar.html') }}">
             <i class="fa fa-calendar"></i> <span>Calendar</span>
             <span class="pull-right-container">
               <small class="label pull-right bg-red">3</small>
@@ -430,7 +430,7 @@
           </a>
         </li>
         <li>
-          <a href="pages/mailbox/mailbox.html">
+          <a href="{{ url('adminlte/pages/mailbox/mailbox.html') }}">
             <i class="fa fa-envelope"></i> <span>Mailbox</span>
             <span class="pull-right-container">
               <small class="label pull-right bg-yellow">12</small>
@@ -447,15 +447,15 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/examples/invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
-            <li><a href="pages/examples/profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
-            <li><a href="pages/examples/login.html"><i class="fa fa-circle-o"></i> Login</a></li>
-            <li><a href="pages/examples/register.html"><i class="fa fa-circle-o"></i> Register</a></li>
-            <li><a href="pages/examples/lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-            <li><a href="pages/examples/404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
-            <li><a href="pages/examples/500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
-            <li><a href="pages/examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-            <li><a href="pages/examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/invoice.html') }}"><i class="fa fa-circle-o"></i> Invoice</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/profile.html') }}"><i class="fa fa-circle-o"></i> Profile</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/login.html') }}"><i class="fa fa-circle-o"></i> Login</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/register.html') }}"><i class="fa fa-circle-o"></i> Register</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/lockscreen.html') }}"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/404.html') }}"><i class="fa fa-circle-o"></i> 404 Error</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/500.html') }}"><i class="fa fa-circle-o"></i> 500 Error</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/blank.html') }}"><i class="fa fa-circle-o"></i> Blank Page</a></li>
+            <li><a href="{{ url('adminlte/pages/examples/pace.html') }}"><i class="fa fa-circle-o"></i> Pace Page</a></li>
           </ul>
         </li>
         <li class="treeview">
